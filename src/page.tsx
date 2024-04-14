54b99f81cfdaf486ca98b649636419d82c3b9ee1
Yu-huan Kuo <touch@rnmeow.com>
2024-04-14 21:02:22 +0800

    init repository

---
+++ src/page.tsx
@@ -0,0 +1,43 @@
+/** @jsx h */
+import { h } from 'htm'
+
+export const PageHead = {
+  lang: 'en',
+  title: "It's Parkin'",
+  meta: {
+    description: 'This website is UNDER DEVELOPMENT.',
+  },
+  links: [
+    { rel: 'preconnect', href: 'https://cdn.jsdelivr.net', crossorigin: '' },
+    { rel: 'dns-prefetch', href: 'https://cdn.jsdelivr.net' },
+    {
+      rel: 'shortcut icon',
+      href: 'https://img.rnmeow.com/favicon.svg',
+      type: 'image/svg+xml',
+    },
+    {
+      rel: 'stylesheet',
+      href: 'https://cdn.jsdelivr.net/npm/simpledotcss@2.3.0/simple.min.css',
+    },
+  ],
+}
+
+export const PageBody = (req: Request) => (
+  <div class='cont'>
+    <h1>Hello World!</h1>
+    <p>
+      You're visiting {(new URL(req.url)).hostname} and the domain is owned by
+      {' '}
+      <a href='https://nekohuan.cyou'>rnmeow</a>.
+      <br />
+      It is currently under development. Feel free to check for updates anytime!
+    </p>
+    <p>
+      The source of this page is available{' '}
+      <a href='https://github.com/rnmeow/parkin'>here</a>.
+    </p>
+    <footer>
+      &copy; {new Date().getFullYear()}, Yu-huan Kuo.
+    </footer>
+  </div>
+)
