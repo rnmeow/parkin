3b88127cf5a27c47ed8714e1cd47367c66ca189d
Yu-huan Kuo <touch@rnmeow.com>
2024-05-01 19:20:30 +0800

    correct typings

---
+++ pages/index.tsx
@@ -0,0 +1,45 @@
+/** @jsx h */
+import { h } from "htm";
+
+// deno-lint-ignore no-explicit-any
+export const indxHead: Record<string, any> = {
+  lang: "en",
+  title: "It's Parkin'",
+  meta: {
+    viewport: "width=device-width, initial-scale=1, shrink-to-fit=no",
+    description: "This website is UNDER DEVELOPMENT.",
+  },
+  links: [
+    { rel: "preconnect", href: "https://unpkg.com", crossorigin: "" },
+    { rel: "dns-prefetch", href: "https://unpkg.com" },
+    {
+      rel: "shortcut icon",
+      href: "https://img.rnmeow.com/favicon.svg",
+      type: "image/svg+xml",
+    },
+    {
+      rel: "stylesheet",
+      href: "https://unpkg.com/simpledotcss@2.3.0/simple.min.css",
+    },
+  ],
+};
+
+export const indxBody = (req: Request) => (
+  <div>
+    <h1>Hello World!</h1>
+    <p>
+      You're visiting <code>{new URL(req.url).hostname}</code> and the domain is
+      owned by <a href="https://nekohuan.cyou">@rnmeow</a>.
+      <br />
+      It is currently under development. Feel free to check for updates anytime!
+    </p>
+    <p>
+      The source of this page is available{" "}
+      <a href="https://github.com/rnmeow/parkin" title="GitHub repo">
+        here
+      </a>
+      .
+    </p>
+    <footer>&copy; {new Date().getFullYear()}, Connor Kuo.</footer>
+  </div>
+);
