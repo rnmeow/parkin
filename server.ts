c0c749a67e3e9162db3c76b39d73ca2c3f8f16f9
Yu-huan Kuo <touch@rnmeow.com>
2024-05-03 20:56:39 +0800

    indent `server.ts`

--- server.ts
+++ server.ts
@@ -2,4 +2,9 @@ import html from 'htm'
 
 import { indexBody, indexHead } from './pages/index.tsx'
 
-Deno.serve((req) => html({ ...indexHead, body: indexBody(req) }))
+Deno.serve((req) =>
+  html({
+    ...indexHead,
+    body: indexBody(req),
+  })
+)
